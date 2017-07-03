d3ba34a20107d4863b278ec4540502fca16c9703
fxhy <hein.fa3@gmail.com>
2017-07-03 19:46:52 +0200

    speicherfenster optimiert

--- Diagrammorama/Diagrammorama/Form2.Designer.cs
+++ Diagrammorama/Diagrammorama/Form2.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.Speichern = new System.Windows.Forms.Button();
             this.plot1 = new OxyPlot.WindowsForms.PlotView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Speichern
             // 
-            this.Speichern.Location = new System.Drawing.Point(12, 12);
+            this.Speichern.Location = new System.Drawing.Point(3, 3);
             this.Speichern.Name = "Speichern";
-            this.Speichern.Size = new System.Drawing.Size(75, 23);
+            this.Speichern.Size = new System.Drawing.Size(92, 23);
             this.Speichern.TabIndex = 5;
             this.Speichern.Text = "Speichern";
             this.Speichern.UseVisualStyleBackColor = true;
@@ -45,22 +47,42 @@
             // plot1
             // 
             this.plot1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.plot1.Location = new System.Drawing.Point(0, 0);
+            this.plot1.Location = new System.Drawing.Point(0, 30);
             this.plot1.Margin = new System.Windows.Forms.Padding(0);
             this.plot1.Name = "plot1";
-            this.plot1.Size = new System.Drawing.Size(632, 446);
+            this.plot1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plot1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.plot1.Size = new System.Drawing.Size(717, 397);
             this.plot1.TabIndex = 0;
-            this.Controls.Add(this.plot1);
+            this.plot1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plot1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plot1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Speichern, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.plot1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(717, 427);
+            this.tableLayoutPanel1.TabIndex = 6;
             // 
             // Diagramm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(717, 427);
-            this.Controls.Add(this.Speichern);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Diagramm";
             this.Text = "Diagramm";
             this.Load += new System.EventHandler(this.Diagramm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -68,5 +90,6 @@
         #endregion
         private System.Windows.Forms.Button Speichern;
         private OxyPlot.WindowsForms.PlotView plot1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
